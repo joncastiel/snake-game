df17f6c45a2af1ace72f4e4ba5ce32b6b59d6270
joncastiel <joncastiel26@gmail.com>
2026-01-01 13:35:45 -0300

    melhorei o pulo do mario

--- js/script.js
+++ js/script.js
@@ -13,10 +13,18 @@ const jump = () => {
 const loop = setInterval(() => {
 
     const pipePosition = pipe.offsetLeft;
+    const marioposition =+window.getComputedStyle(mario).bottom.replace('px', '');
 
-    if (pipePosition <= 120) {
+
+    
+    console.log(marioposition);
+
+    if (pipePosition <= 120 && pipePosition > 0 && marioposition < 80) {
         pipe.style.animation = 'none';
         pipe.style.left = `${pipePosition}px`;
+
+        mario.style.animation = 'none';
+        mario.style.bottom = `${marioposition}px`;
     }
 
 }, 10)
