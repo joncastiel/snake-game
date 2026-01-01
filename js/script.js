d221035af0f2db54613745b9e995f51a4bf727b1
joncastiel <joncastiel26@gmail.com>
2026-01-01 13:07:46 -0300

    trabalhando no pulo

--- js/script.js
+++ js/script.js
@@ -1,29 +1,29 @@
 const mario = document.querySelector('.mario')
+const pipe = document.querySelector('.pipe')
 
 const jump = () => {
     mario.classList.add('jump')
-    
+
     setTimeout(() => {
         mario.classList.remove('jump')
     }, 500)
 }
 
-window.addEventListener('keydown', (event) => {
+
+const loop = setInterval(() => {
+
+    const pipePosition = pipe.offsetLeft;
+
+    if (pipePosition <= 120) {
+        pipe.style.animation = 'none';
+        pipe.style.left = `${pipePosition}px`;
+    }
+
+}, 10)
+
+
+document.addEventListener('keydown', (event) => {
     if (event.key === 'ArrowUp' || event.keyCode === 38) {
         jump()
     }
 })
-
-
-
-
-
-
-
-
-
-
-
-
-
-
